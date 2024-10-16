d410341b7da8470efbca93f88a7c26781c76bd4f
Damian X <dx4336969@gmail.com>
2024-10-16 14:37:22 +0000

    implementing signup with jwt tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/userModel");
 const APIFeatures = require("../utils/APIFeatures");
 const catchAsync = require("./catchAsync");
 const AppError = require("../utils/AppError");
+const jwt = require('jsonwebtoken')
 
 exports.getAllUsers = catchAsync(async(req,res,next)=>{
     const features = new APIFeatures(User.find(),req.query).sort().field().limit().paginate()
@@ -43,11 +44,13 @@ exports.getUser = catchAsync(async(req,res,next)=>{
 
 exports.signup = catchAsync(async(req,res,next)=>{
     const {name,email,password,passwordConfirm} = req.body
-    const user = await User.create({name,password,passwordConfirm,email})
+    const newUser = await User.create({name,password,passwordConfirm,email})
+    const token = jwt.sign({id:newUser.__id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES_IN})
     res.status(200).json({
         status: 'success',
+        token,
         data: {
-            user
+            newUser
         }
     })
 
