29a9ae8099daaa2ce708eac5603606dbdd573185
Damian X <dx4336969@gmail.com>
2024-10-16 11:02:19 +0000

    handling exceptions and rejections

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
+process.on('uncaughtException',err=>{
+    console.log('uncaught Exception! ✴️ shutting down......')
+    console.log(err.name,err.message)
+    process.exit(1)
+})
 dotenv.config({path:'./config.env'})
+const app = require('./app');
 const port = process.env.PORT || 8000
 
+    
 const DB = process.env.DATABASE_LOCAL
 mongoose.connect(DB).then(con=>{
     console.log('DB connection successful')
 })
-
-app.listen(port, ()=>{
+const server = app.listen(port, ()=>{
     console.log(`App running on port ${port}`)
 })
+
+process.on('unhandledRejection',err=>{
+    console.log('unhandled Rejection! ✴️ shutting down......')
+    console.log(err.name,err.message)
+    server.close(()=>{
+        process.exit(1)
+    })
+    })
+
+    console.log(x)
