1335eae348ef5c21c400b43b6e467b73aba3c7a5
Damian X <dx4336969@gmail.com>
2024-10-16 10:43:36 +0000

    handling prod and development errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,36 @@
-module.exports = (err, req, res,next) => {
-    
-    err.statusCode = err.StatusCode || 404;
-    err.status = err.status || 'fail'
+const { stack } = require("../app");
+const AppError = require("../utils/AppError");
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
-    })
-    
-    
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({});
   }
+};
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const handleCastError= (err)=>{
+    const message = `invalid ${err.path}: ${err.value}.`
+    return new AppError(message,400)
+}
+module.exports = (err, req, res, next) => {
+  
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = {...err}
+    if(err.name === 'CastError') error = handleCastError(error)
+    sendErrorProd(error, res);
+  }
+};
