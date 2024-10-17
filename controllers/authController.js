c34c82ecdbb40c785bcb70c87864b1a14493d42f
Damian X <dx4336969@gmail.com>
2024-10-17 14:11:30 +0000

    fix jwt decode bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,6 +53,7 @@ exports.signup = catchAsync(async(req,res,next)=>{
     const {name,email,password,passwordConfirm} = req.body
     const newUser = await User.create({name,password,passwordConfirm,email})
     const token = signToken(newUser)
+    
     res.status(200).json({
         status: 'success',
         token,
@@ -90,25 +91,24 @@ exports.login = catchAsync(async(req,res,next)=>{
 exports.protect = catchAsync(async(req,res,next)=>{
     let token;
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer') ){
-        token = req.headers.authorization.split(' ')[1].trim()
+        token = req.headers.authorization.split(' ')[1]
 
     }
     if(!token)return next(new AppError('You are not logged in! please login to access route:D',401))
-        const decoded = await jwt
-    .verify(token,process.env.JWT_SECRET)
-console.log('token',decoded)
-console.log('token',process.env.JWT_SECRET)
+        const decoded = await promisify(jwt
+    .verify)(token,process.env.JWT_SECRET)
 
 
-    const currentUser = await User.findById(decoded
-    )
+
+    const currentUser = await User.findById(decoded.id)
     if(!currentUser){
         return next(new AppError('The user belonging to this token no longer exist',401))
     }
-    if(currentUser.changedPasswordAfter(decoded.iat)){
+    if(currentUser.passwordChangedAfter(decoded.iat)){
         return next(new AppError('User recently changed password please login again',401))
 
     }
     req.user = currentUser
     next()
 })
+
