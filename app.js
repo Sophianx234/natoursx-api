10556d519cb14302499282d284626c7f68410e24
Damian X <dx4336969@gmail.com>
2024-10-21 09:58:58 +0000

    implementing data sanitization

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const helmet = require('helmet');
 
+const errorController = require("./controllers/errorController");
 const tourRouter = require("./routes/ToursRoute");
 const userRouter = require("./routes/userRoute");
 const AppError = require("./utils/AppError");
-const errorController = require("./controllers/errorController");
 const rateLimit = require('express-rate-limit')
-const helmet = require('helmet');
+
+
 const limit  = rateLimit({
   max:100,
   windowMS:60*60*1000,
   message: 'Too many request from this IP! please try again in an hour!'
 })
 app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
 app.use('/api',limit)
 app.use(morgan("dev"));
 app.use(express.json({limit: '10kb'}));
