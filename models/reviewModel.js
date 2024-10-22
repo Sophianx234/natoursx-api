e70ba8dff57c8d22fe55e9f4797f8204c2bf8dce
Damian X <dx4336969@gmail.com>
2024-10-22 13:25:02 +0000

    add review model

---
+++ models/reviewModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type:String,
+        required: [true, 'review cannot be empty']
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+
+    },
+    createdAt: Date,
+    tour:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'review must belong to a tour']
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    },
+    
+},
+{
+    toJSON: {virtuals:true},
+    toObject: {virtuals: true}
+})
+
+const Review = mongoose.model('Review',reviewSchema)
+
+module.exports = Review
