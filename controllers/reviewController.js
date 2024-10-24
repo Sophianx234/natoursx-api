2d13a445e040cdd023ecf44669fa877a8e61343f
Damian X <dx4336969@gmail.com>
2024-10-24 11:19:37 +0000

    implementing nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -34,6 +34,8 @@ exports.getReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async(req,res,next)=>{
+  if(!req.body.user) req.body.user = req.user.id
+  if(!req.body.tour) req.body.tour = req.params.tourId
   const newReview = await Review.create(req.body)
 
   res.status(200).json({
--- routes/ToursRoute.js
+++ routes/ToursRoute.js
@@ -2,11 +2,19 @@ const express = require("express");
 const tourController = require("./../controllers/ToursController");
 const router = express.Router();
 
-const {protect,restrictTo} = require('./../controllers/authController')
+ 
+const reviewController = require("../controllers/reviewController");
+const authController = require("../controllers/authController");
 const { getAllTours,getTour,aliasTopTours,updateTour,deleteTour,createTour } = tourController;
+const {createReview} = reviewController
+const {protect,restrictTo} = authController
+
+
+
 router.route('/top-5-cheap').get(aliasTopTours,getAllTours)
 router.route("/").get(protect,getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(protect,restrictTo('admin','lead-guide'),deleteTour)
+router.route('/:tourId/reviews').post(protect,createReview)
 
 
 module.exports = router;
